5f99e680e44ebec2d791f4758e5f18344cf9b4fe
Hyungjun Lim <sculd3@gmail.com>
2021-03-27 10:06:11 -0400

    log for debugging

--- src/components/signaltable.js
+++ src/components/signaltable.js
@@ -143,6 +143,7 @@ function SignalTable() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
+  console.log(process.env.REACT_APP_API_GATEWAY_API_KEY)
   useEffect(() => {
     fetch(
       "https://7tj23qrgl1.execute-api.us-east-2.amazonaws.com/test/moves/",
